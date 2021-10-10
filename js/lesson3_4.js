0ea6d0af128f984c5e7360c62050c396008baa9d
s-k-git <89926320+s-k-git@users.noreply.github.com>
2021-10-10 23:56:14 +0900

    JavaScript課題③-4のレビュー依頼

--- js/lesson3_4.js
+++ js/lesson3_4.js
@@ -5,21 +5,19 @@ const comment = document.getElementById("comment");
 const table = document.getElementById("table-contents");
 const changetask = document.getElementsByName("change");
 
+const radioall = document.getElementById("radio-all");
+const radioworking = document.getElementById("radio-working");
+const radiofinish = document.getElementById("radio-finish");
+
 const addtable = () => {
     const todo = { task: comment.value, status: "作業中", deletion: "削除" };
     todos.push(todo);
-    if (changetask[1].checked) {
-        listdescription1();
-    } else if (changetask[2].checked) {
-        listdescription2();
-    } else {
-        listdescription();
-    }
+    listdescription(todos);
 }
 
-function listdescription() {
+function listdescription(arr) {
     table.innerHTML = "";
-    todos.forEach(function (value, index) {
+    arr.forEach(function (value, index) {
         const statusbtn = document.createElement("input");
         statusbtn.type = "button";
         statusbtn.value = value.status;
@@ -45,124 +43,40 @@ function listdescription() {
 
         deletebtn.addEventListener("click", () => {
             todos.splice(index, 1);
-            listdescription();
+            listdescription(arr);
         });
 
         statusbtn.addEventListener("click", () => {
             if (value.status === "作業中") {
                 todos[index].status = "完了";
-                listdescription();
+                listdescription(arr);
             } else {
                 todos[index].status = "作業中";
-                listdescription();
+                listdescription(arr);
             }
         });
     });
     comment.value = "";
 }
 
-const listdescription1 = () => {
-    table.innerHTML = "";
-    todos.forEach(function (value, index) {
-        const statusbtn = document.createElement("input");
-        statusbtn.type = "button";
-        statusbtn.value = value.status;
+const filtertodos = () => {
+    if (radioall.checked) {
+        return listdescription(todos);
+    } else if (radioworking.checked) {
+        const workingtodos = todos.filter(function (value) {
+            return value.status === "作業中"
+        })
+        return listdescription(workingtodos);
+    } else if (radiofinish.checked) {
+        const finishtodos = todos.filter(function (value) {
+            return value.status === "完了"
+        })
+        return listdescription(finishtodos);
+    }
+};
 
-        const deletebtn = document.createElement("input");
-        deletebtn.type = "button";
-        deletebtn.value = value.deletion;
-
-        if (statusbtn.value === "作業中") {
-
-            let newRow = table.insertRow();
-            let newCell = newRow.insertCell();
-            let newText = document.createTextNode(index);
-            newCell.appendChild(newText);
-
-            newCell = newRow.insertCell();
-            newText = document.createTextNode(value.task);
-            newCell.appendChild(newText);
-
-            newCell = newRow.insertCell();
-            newCell.appendChild(statusbtn);
-
-            newCell = newRow.insertCell();
-            newCell.appendChild(deletebtn);
-
-            deletebtn.addEventListener("click", () => {
-                todos.splice(index, 1);
-                listdescription();
-            });
-
-            statusbtn.addEventListener("click", () => {
-                if (value.status === "作業中") {
-                    todos[index].status = "完了";
-                    listdescription();
-                } else {
-                    todos[index].status = "作業中";
-                    listdescription();
-                }
-            });
-        }
+changetask.forEach(function (value,index) {
+        changetask[index].addEventListener("click", () => {
+            filtertodos();
+        });
     });
-    comment.value = "";
-}
-
-const listdescription2 = () => {
-    table.innerHTML = "";
-    todos.forEach(function (value, index) {
-        const statusbtn = document.createElement("input");
-        statusbtn.type = "button";
-        statusbtn.value = value.status;
-
-        const deletebtn = document.createElement("input");
-        deletebtn.type = "button";
-        deletebtn.value = value.deletion;
-
-        if (statusbtn.value === "完了") {
-
-            let newRow = table.insertRow();
-            let newCell = newRow.insertCell();
-            let newText = document.createTextNode(index);
-            newCell.appendChild(newText);
-
-            newCell = newRow.insertCell();
-            newText = document.createTextNode(value.task);
-            newCell.appendChild(newText);
-
-            newCell = newRow.insertCell();
-            newCell.appendChild(statusbtn);
-
-            newCell = newRow.insertCell();
-            newCell.appendChild(deletebtn);
-
-            deletebtn.addEventListener("click", () => {
-                todos.splice(index, 1);
-                listdescription();
-            });
-
-            statusbtn.addEventListener("click", () => {
-                if (value.status === "作業中") {
-                    todos[index].status = "完了";
-                    listdescription();
-                } else {
-                    todos[index].status = "作業中";
-                    listdescription();
-                }
-            });
-        }
-    });
-    comment.value = "";
-}
-
-changetask[1].addEventListener("click", () => {
-    listdescription1();
-});
-
-changetask[2].addEventListener("click", () => {
-    listdescription2();
-});
-
-changetask[0].addEventListener("click", () => {
-    listdescription();
-});
