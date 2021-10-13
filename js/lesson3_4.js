ade6fc16918ef126365fdf2982e7bf60b930aad5
s-k-git <89926320+s-k-git@users.noreply.github.com>
2021-10-13 20:01:12 +0900

    JavaScript課題③-4のレビュー依頼

--- js/lesson3_4.js
+++ js/lesson3_4.js
@@ -12,12 +12,13 @@ const radiofinish = document.getElementById("radio-finish");
 const addtable = () => {
     const todo = { task: comment.value, status: "作業中", deletion: "削除" };
     todos.push(todo);
-    listdescription(todos);
+    listdescription();
+    sort();
 }
 
-function listdescription(arr) {
+function listdescription() {
     table.innerHTML = "";
-    arr.forEach(function (value, index) {
+    todos.forEach(function (value, index) {
         const statusbtn = document.createElement("input");
         statusbtn.type = "button";
         statusbtn.value = value.status;
@@ -41,42 +42,59 @@ function listdescription(arr) {
         newCell = newRow.insertCell();
         newCell.appendChild(deletebtn);
 
+        if (value.status === "作業中") {
+            newRow.classList.add("working");
+        } else {
+            newRow.classList.add("finished");
+        }
+
         deletebtn.addEventListener("click", () => {
             todos.splice(index, 1);
-            listdescription(arr);
+            listdescription();
         });
 
         statusbtn.addEventListener("click", () => {
             if (value.status === "作業中") {
                 todos[index].status = "完了";
-                listdescription(arr);
+                listdescription();
             } else {
                 todos[index].status = "作業中";
-                listdescription(arr);
+                listdescription();
             }
         });
     });
     comment.value = "";
 }
 
-const filtertodos = () => {
-    if (radioall.checked) {
-        return listdescription(todos);
-    } else if (radioworking.checked) {
-        const workingtodos = todos.filter(function (value) {
-            return value.status === "作業中"
-        })
-        return listdescription(workingtodos);
-    } else if (radiofinish.checked) {
-        const finishtodos = todos.filter(function (value) {
-            return value.status === "完了"
-        })
-        return listdescription(finishtodos);
-    }
-};
-
-changetask.forEach(function (value,index) {
-        changetask[index].addEventListener("click", () => {
-            filtertodos();
-        });
+changetask.forEach(function(value,index){
+    changetask[index].addEventListener("click", () => {
+        sort();
     });
+});
+
+    const sort = () => {
+        const finished = document.getElementsByClassName("finished");
+        const working = document.getElementsByClassName("working");
+        if (changetask[1].checked) {
+            for (let i = 0; i < finished.length; i++) {
+                finished[i].style.display = "none";
+            };
+            for (let j = 0; j < working.length; j++) {
+                working[j].style.display = "table-row";
+            };
+        } else if (changetask[2].checked) {
+            for (let j = 0; j < working.length; j++) {
+                working[j].style.display = "none";
+            };
+            for (let i = 0; i < finished.length; i++) {
+                finished[i].style.display = "table-row";
+            };
+        } else {
+            for (let j = 0; j < working.length; j++) {
+                working[j].style.display = "table-row";
+            };
+            for (let i = 0; i < finished.length; i++) {
+                finished[i].style.display = "table-row";
+            };
+        }
+    } 
